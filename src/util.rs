d9ad696fd85295fbf9c050f2385c15ff3b697101
Brian "Beej Jorgensen" Hall <beej@beej.us>
2025-12-11 17:04:12 -0800

    Remove unused file

--- src/util.rs
+++
@@ -1,8 +0,0 @@
-/// Safely add an i64 to a u64
-fn apply_delta(value: u64, delta: i64) -> u64 {
-    if delta >= 0 {
-        value.saturating_add(delta as u64)
-    } else {
-        value.saturating_sub((-delta) as u64)
-    }
-}
