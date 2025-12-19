a87c126bf7f23447647a2fd8ee9dff9145acd525
Brian "Beej Jorgensen" Hall <beej@beej.us>
2025-12-18 20:40:11 -0800

    Allow repeated stock splitting

--- src/starlanes.rs
+++ src/starlanes.rs
@@ -483,7 +483,17 @@ impl StarLanes {
     fn stock_split(&mut self, co_num: usize, events: &mut Vec<Event>) {
         let company = &mut self.companies[co_num];
 
-        if company.share_price > DEFAULT_STOCK_SPLIT_LIMIT {
+        // In the original game, the stock could only split once per
+        // move or per merge event. This was mathematical; the companies
+        // had a maximum stock price of 3000 before splitting, so
+        // joining two would make for a price of 6000, which would then
+        // split 2-for-1 to get back to 3000.
+        //
+        // However, if we allow tuning of the parameters, it could be
+        // that the stock would split repeatedly before it got below the
+        // limit. As such, this is a while loop instead of an if
+        // statement.
+        while company.share_price > DEFAULT_STOCK_SPLIT_LIMIT {
             // Price is halved
             company.share_price /= DEFAULT_STOCK_SPLIT_FACTOR as u64;
 
